2e2c0cabb64d1aec0bec6df82fe467f4219c71c7
Azdren Ymeri <azdreny@gmail.com>
2019-10-14 15:41:37 +0200

    big.js added, operate.js done

---
+++ src/components/logic/operate.js
@@ -0,0 +1,19 @@
+import Big from 'big-js';
+
+const operate = (numberOne, numberTwo, operator) => {
+
+    switch(operator) {
+        case '+':
+            return new Big(numberOne + numberTwo);
+        case '-':
+            return new Big(numberOne - numberTwo);
+        case 'x':
+            return new Big(numberOne * numberTwo);
+        case '÷':
+            return new Big(numberOne / numberTwo);
+        case '%':
+            return new Big(numberOne % numberTwo);
+        default:
+            return "Error";
+    }
+}
