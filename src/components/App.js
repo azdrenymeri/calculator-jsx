33e0ca9a3b95d5ba25b48e21c044f7d78d38f2d6
Azdren <azdreny@gmail.com>
2019-10-19 13:08:38 +0200

    small display fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import './../styles/App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from './logic/calculate';
+import calculate from './../logic/calculate';
 
 class App extends Component {
   constructor(){
@@ -18,13 +18,13 @@ class App extends Component {
   handleClick(event){
     const buttonName = event.target.textContent
     const{total,next,operation} = calculate(this.state,buttonName);
-    this.setState({total:total,next:next,operation:operation});
+    this.setState({total:total, next:next, operation:operation});
   }
 
   render(){
     return (
       <div className="App">
-        <Display result={this.state.total} />
+        <Display result={this.state.next? this.state.next: this.state.total} />
         <ButtonPanel clickHandler={this.handleClick}  />
       </div>
     );
